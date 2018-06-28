3d9976b1ede286cc5e6311f97b579db45040865c
Kolbjorn <kolbjcrn@gmail.com>
2018-06-28 17:57:10 -0400

    can now broadcast to all clients

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <limits.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,6 +12,12 @@
 
 #define PORTNUM 9002
 #define MAX_BUFFER_LEN 500
+//#define CLIENT_CHUNK 10
+
+//int *clientList;
+//int clientRoof;
+int clientCeil;
+int clientFloor;
 
 void *listenForMessages(void *args)
 {
@@ -26,12 +33,23 @@ void *listenForMessages(void *args)
 			//TODO: add sleep to test buffer message stack
 			buffer[messageSize] = '\0';
 			printf("Message(%d):: %s (%d bytes).\n", clientSock, buffer, messageSize);
+			
+			printf("Broadcast from %d to %d\n", clientFloor, clientCeil);
+			for (int i = clientFloor; i < clientCeil+1; i++)
+			{
+				write(i, buffer, messageSize);
+			}
+			
 			memset(buffer, '\0', sizeof(buffer));//emtpy buffer
 		}
-		else if(messageSize == 0)
+		else if (messageSize == 0)
 		{
 			printf("Client %d disconnected\n", clientSock);
 			break;
+		}
+		else if (messageSize == -1)
+		{
+			printf("Receive failed\n");
 		}
 	}
 
@@ -53,6 +71,13 @@ struct sockaddr_in getServerAddressInfo()
 int main(int argc, char * argv[])
 {
 	printf("Server init\n");
+
+//	clientList = malloc(CLIENT_CHUNK * sizeof(int));
+//	clientRoof = CLIENT_CHUNK;
+//	clientCounter = 0;
+
+	clientCeil = 0;
+	clientFloor = INT_MAX;
 
 	struct sockaddr_in serverInfo = getServerAddressInfo();
 
@@ -79,7 +104,22 @@ int main(int argc, char * argv[])
 	{
 		currentClientSock = accept(serverSock, (struct sockaddr*) &clientInfo, &sockSize);
 		printf("Established connection with %s\n", inet_ntoa(clientInfo.sin_addr));
-		
+	
+//		clientList[clientCounter++] = currentClientSock;
+//		if (clientCounter >= clientRoof-1)
+//		{
+//			clientRoof += CLIENT_CHUNK;
+//			clientList = realloc(clientList, clientRoof);
+//			printf("Reallocated clientlist, roof is now at %d\n", clientRoof);
+//		}
+//		printf("Added %d to clientList\n", currentClientSock);
+
+		if (currentClientSock > clientCeil)
+			clientCeil = currentClientSock;
+
+		if (currentClientSock < clientFloor)
+			clientFloor = currentClientSock;
+
 		pthread_t listenerThread;
 		newClientSock = malloc(1);
 		*newClientSock = currentClientSock;
@@ -89,6 +129,7 @@ int main(int argc, char * argv[])
 	//TODO:join threads?
 
 	close(serverSock);
+//	free(clientList);
 
 	return 0;
 }
