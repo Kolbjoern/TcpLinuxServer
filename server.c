3bc8c6cb31ef69f6fa9bd35d3ee3246f0b77c140
Kolbjorn <kolbjcrn@gmail.com>
2018-07-10 14:20:30 -0400

    coding style change

--- server.c
+++ server.c
@@ -19,21 +19,19 @@ int clientCounter;
 
 void removeElement(int *array, int index, int arrayLength)
 {
-	for (int i = index; i < arrayLength-1; i++)
-	{
-		array[i] = array[i + 1];
+ 	for (int i = index; i < arrayLength-1; i++) {
+   		array[i] = array[i + 1];
 	}
 }
 
 int getClientIndex(int *array, int client, int arrayLength)
 {
-	for (int i = 0; i < arrayLength; i++)
-	{
+	for (int i = 0; i < arrayLength; i++) {
 		if (client == array[i]){
 			return i;
 		}
 	}
-	return -1;
+ 	return -1;
 }
 
 void *listenForMessages(void *args)
@@ -42,30 +40,25 @@ void *listenForMessages(void *args)
 	int messageSize;
 	int buffer[MAX_BUFFER_LEN + 1]; // +1 for null terminator
 
-	while (1)
-	{
+	while (1) {
 		messageSize = recv(clientSock, buffer, MAX_BUFFER_LEN, 0);
-		if (messageSize > 0)
-		{
+		if (messageSize > 0) {
 			//TODO: add sleep to test buffer message stack
 			buffer[messageSize] = '\0';
 			printf("Message(%d):: %s (%d bytes).\n", clientSock, buffer, messageSize);
-			
+
 			printf("Broadcast from to entire client list (%d)\n", clientCounter);
-			for (int i = 0; i < clientCounter; i++)
-			{
+			for (int i = 0; i < clientCounter; i++) {
 				write(clientList[i], buffer, messageSize);
 			}
-			
+
 			memset(buffer, '\0', sizeof(buffer));//emtpy buffer
 		}
-		else if (messageSize == 0)
-		{
+		else if (messageSize == 0) {
 			printf("Client %d disconnected\n", clientSock);
-			
+
 			int clientIndex = getClientIndex(clientList, clientSock, clientCounter);
-			if (clientIndex == -1)
-			{
+			if (clientIndex == -1) {
 				printf("Could not remove client %d\n", clientSock);
 				break;
 			}
@@ -103,14 +96,16 @@ int main(int argc, char * argv[])
 
 	// create tcp socket, with IP version 4
 	int serverSock = socket(AF_INET, SOCK_STREAM, 0); //socket for incoming connections
-	if (serverSock == -1)
+	if (serverSock == -1) {
 		printf("Could not create socket\n");
+	}
 
 	// bind server information to socket
 	int bindResponse;
 	bindResponse = bind(serverSock, (struct sockaddr*) &serverInfo, sizeof(struct sockaddr));
-	if (bindResponse < 0)
+	if (bindResponse < 0){
 		printf("Could not bind socket\n");
+	}
 
 	socklen_t sockSize = sizeof(struct sockaddr_in);
 	struct sockaddr_in clientInfo; // socket info about the connecting client
@@ -120,14 +115,12 @@ int main(int argc, char * argv[])
 	int currentClientSock;
 	int *newClientSock;
 
-	while (1)
-	{
+	while (1) {
 		currentClientSock = accept(serverSock, (struct sockaddr*) &clientInfo, &sockSize);
 		printf("Established connection with %s\n", inet_ntoa(clientInfo.sin_addr));
-	
+
 		clientList[clientCounter++] = currentClientSock;
-		if (clientCounter >= clientRoof-1)//remove -1?
-		{
+		if (clientCounter >= clientRoof-1) {//remove -1?
 			clientRoof += CLIENT_CHUNK;
 			clientList = realloc(clientList, clientRoof);
 			printf("Reallocated clientlist, roof is now at %d\n", clientRoof);
@@ -146,5 +139,5 @@ int main(int argc, char * argv[])
 	close(serverSock);
 	free(clientList);
 
-	return 0;
+ 	return 0;
 }
